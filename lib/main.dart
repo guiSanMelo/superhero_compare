b03828e252a829cb8b0c7c44167f18421bd17b6c
guiSanMelo <guilherme.ilhotas2004@hotmail.com>
2026-05-11 17:56:54 -0300

    Começando a fazer o routes das telas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:superhero_compare/screens/home_page.dart';
-
+import 'package:superhero_compare/pages/home_page.dart';
+import 'package:superhero_compare/pages/home.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const Home(),
     );
   }
 }
